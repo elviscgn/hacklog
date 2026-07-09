2f88eb5a41c688776ef2771a1db6742b73ef0b6d
Elvis Chege <96030189+elviscgn@users.noreply.github.com>
2026-07-09 12:56:42 +0000

    fix banner logo backslash escape bug

--- src/banner.c
+++ src/banner.c
@@ -1,30 +1,20 @@
 #include "banner.h"
 #include <string.h>
 
-/* clang-format off */
 static const char *banner_big =
     "                      _       __                \n"
     "  /\\  /\\  __ _   ___ | | __  / /    ___    __ _ \n"
     " / /_/ / / _` | / __|| |/ / / /    / _ \\  / _` |\n"
     "/ __  / | (_| || (__ |   < / /___ | (_) || (_| |\n"
-    "\\/ /_/   \\__,_| \\___||_|\\_\\\\\\\\____/  \\___/  \\__, |\n"
+    "\\/ /_/   \\__,_| \\___||_|\\_\\\\____/  \\___/  \\__, |\n"
     "                                          |___/ \n";
-/* clang-format on */
 
 static const char *banner_small = "[ hacklog ]";
 
-const char *banner_get(void) {
-    return banner_big;
-}
+const char *banner_get(void) { return banner_big; }
 
-const char *banner_get_small(void) {
-    return banner_small;
-}
+const char *banner_get_small(void) { return banner_small; }
 
-int banner_height(void) {
-    return 6;
-}
+int banner_height(void) { return 6; }
 
-int banner_width(void) {
-    return 48;
-}
+int banner_width(void) { return 48; }
